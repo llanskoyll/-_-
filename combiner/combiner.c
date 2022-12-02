e1221e4753ef4fab0e50e46cc03939dc7fb289bf
llanskoyll <artem.maximov.2000@bk.ru>
2022-12-02 20:27:08 +0300

    попытка подправить 5 этап

--- combiner/combiner.c
+++ combiner/combiner.c
@@ -1,32 +1,72 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <pigpio.h>
-#include <time.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <pthread.h>
+#include "combiner.h"
 
-// функция обработки кнопок
-void *button_handler(void *argv)
+pthread_mutex_t mutex_button_record = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_time = PTHREAD_MUTEX_INITIALIZER;
+
+extern bool button_record = false;
+extern time_t _time = 0;
+
+// т.к время может быть не "настоящим", а результатом изменений, то сделаем такой костыль
+static void *time_counter(NULL) 
 {
+    sleep(1);
+    phtread_mutex_lock(&mutex_time);
 
+    _time++;
+
+    pthread_mutex_unlock(&mutex_time);
+}
+// функция обработки кнопок
+static void *button_handler(void *argv)
+{
+    int fd = 0;
+    int value_button_record_cur;
+    char value_button_record;
+
+// чтение значение из файла нажатой кнопки
+    fd = fopen(file_name, "r");
+
+    value_button_record = fgetc(file);
+    value_button_record_cur = atoi(value_button_record);
+
+    pthread_mutex_lock(&mutex_button_record);
+
+    // кнопка нажата button_record = true
+    if (value_button_record_cur) {
+        button_record = true;    
+    } else {
+        button_record = false;
+    }
+
+    pthread_mutex_unlock(&mutex_button_record);
+}
+
+static void time_print(int time_sec) 
+{
+    pthread_mutex_lock(&mutex_time);
+
+    _time = _time - time_sec;
+    printf("Время изменено на %ld!\n", _time);
+
+    pthread_mutex_unlock(&mutex_time);
 }
 
 // функция обработки именнованнымии каналов
-void *pipe_handler(void *argv)
+static void *pipe_handler(void *argv)
 {
+    int fd;
+    char str[5];
+    int ret;
+    int value_from_pipe;
+ 
     while(1) {
-        int fd;
-        char str[5];
-        int ret;
-        int value_from_pipe;
-
+        
         char *path = (char *)argv;
-        printf("%s\n", path);
+        fd = 0;
+
+        // printf("%s\n", path);
+
+        // чтение из encoder fifo
         fd = open(path, O_RDONLY);
         if (fd == -1) {
             printf("Failed open to pipe!\n");
@@ -34,15 +74,27 @@ void *pipe_handler(void *argv)
 
         read(fd, (void *)str, sizeof(value_from_pipe));
         close(fd);
-        value_from_pipe = atoi(str);
-        printf("%d\n", value_from_pipe);
+
+        value_from_pipe = (atoi(str))/18;
+
+        pthread_mutex_lock(&mutex_button_record);
+        
+        if (mutex_button_record) {
+            printf("Сигнал изменение времени на %d секунд\n", value_from_pipe);
+            time_print(value_from_pipe);
+        }
+
+        pthread_mutex_unlock(&mutex_button_record);
+
+        // канал для lcd
         mkfifo("lcd_fifo", 0666);
         sprintf(str, "%d", value_from_pipe);
-        fd = 0;
+
         fd = open("lcd_fifo", O_WRONLY);
         if (fd == -1) {
             printf("Failed open pipe for write\n");
         }
+
         ret = write(fd, str, 5);
         if (ret == -1) {
             printf("Failed write to pipe!\n");
@@ -59,16 +111,19 @@ int main(int argc, char **argv)
         printf("Uncorrect count argument !\n");
         return 0;
     }
-    //char path[50];
-    //strncpy(&path[0], &argv[2], sizeof(path)); 
+    _time = time(NULL);
+    
     pthread_t thread_button_handler; // поток для обрботки кнопок
     pthread_t thread_pipe_handler;
+    pthread_t thread_time;
 
-    // запуск поток обработчика кнопок
     pthread_create(&thread_button_handler, NULL, button_handler, NULL);
     pthread_create(&thread_pipe_handler, NULL, pipe_handler, (void *)argv[1]);
+    pthread_create(&thread_time, NULL, time_counter, NULL);
+
     pthread_join(thread_button_handler, NULL);
     pthread_join(thread_pipe_handler, NULL);
+    pthread_join(thread_time, NULL);
 
     return 0;
 }
---
+++ combiner/combiner.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pigpio.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <stdbool.h>
+
+static void *button_handler(void *argv);
+static void *pipe_handler(void *argv);
+static void *time_counter(NULL);
+
+static void time_print(int time_sec);
+
